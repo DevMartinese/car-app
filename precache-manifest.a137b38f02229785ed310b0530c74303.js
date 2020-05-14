eb17db8627d745865aab49bd40a2c0a8a3bc3950
DevMartinese <devmartinese@gmail.com>
2020-05-14 11:20:16 -0300

    Updates

---
+++ precache-manifest.a137b38f02229785ed310b0530c74303.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6984d49f1078bf4e8ab5be7f46a1400",
+    "url": "/car-app/index.html"
+  },
+  {
+    "revision": "f31598d48bc389463ea4",
+    "url": "/car-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fd33175b987221ad0c71",
+    "url": "/car-app/static/js/2.5790396b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/car-app/static/js/2.5790396b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f31598d48bc389463ea4",
+    "url": "/car-app/static/js/main.d7d82894.chunk.js"
+  },
+  {
+    "revision": "beebe083e77bfb334818",
+    "url": "/car-app/static/js/runtime-main.c35022d1.js"
+  },
+  {
+    "revision": "d40352cbf544922f591fe3a6f9edf5a5",
+    "url": "/car-app/static/media/EGO.d40352cb.svg"
+  }
+]);
